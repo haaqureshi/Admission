fd19216b53af0a35ec1ccbf9e6d7e7ba2756edef
namjad40 <namjad@bsolpk.org>
2024-11-10 04:00:46 +0500

    Supabase issue fixed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mqsnsniztjalbciebkkg.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xc25zbml6dGphbGJjaWVia2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2ODE0NTYsImV4cCI6MjAzOTI1NzQ1Nn0.PVZBkjt48makijU34-H-52gSDbvROWkKzB9torNUFtY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
